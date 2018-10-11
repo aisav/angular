bbf4f00bceb54e37d117b4eac12ce6589fc72c14
aisav <artur.isaverdyan@gmail.com>
2018-10-11 23:52:44 +0400

    inline template & style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html',
+  template: `
+    <h1>Inline Template</h1>
+    <app-other></app-other>
+  
+  `,
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    h1 {
+      color:blue;
+    }
+  
+  `]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'app changed!';
 }
